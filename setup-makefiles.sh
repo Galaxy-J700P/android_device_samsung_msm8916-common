4e503191b21becb6705fb32fd8bbae868deb4ac8
minz1 <emerytang@gmail.com>
2017-06-30 21:35:11 -0400

    setup-makefiles: add j7ltespr and j7ltechn to guard

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,7 +21,7 @@ set -e
 INITIAL_COPYRIGHT_YEAR=2017
 
 BOARD_COMMON=msm8916-common
-DEVICES_ALL="gprimelte gprimeltespr gprimeltexx gtelwifiue gtesqltespr"
+DEVICES_ALL="gprimelte gprimeltespr gprimeltexx gtelwifiue gtesqltespr j7ltespr j7ltechn"
 VENDOR=samsung
 
 # Load extractutils and do some sanity checks
